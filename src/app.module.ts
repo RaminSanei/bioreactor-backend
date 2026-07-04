3179cfa4138a8f9643a41ea27ea294181ec56bd0
Ali Sanei <ramin@Alis-MacBook-Air-2.local>
2026-07-04 18:42:33 +0200

    feat: initial setup with telemetry module

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TelemetryModule } from './telemetry/telemetry.module';
+import { TelemetryModule } from './telemetry/telemetry.module';
+
+@Module({
+  imports: [TelemetryModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
